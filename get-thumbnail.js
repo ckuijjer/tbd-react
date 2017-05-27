6ff8025582dc1332a6e889a53e6ccb3711331444
Casper Kuijjer <casper@kuijjer.com>
2017-05-27 10:52:45 +0200

    fetch new kitties

--- get-thumbnail.js
+++ get-thumbnail.js
@@ -1,12 +1,17 @@
-const json = require('./kitty.json');
+const fetch = require('isomorphic-fetch');
 
-const thumbnails = json.data.children.map(child => {
-  // get the first image
-  const image = child.data.preview.images[0];
+const subreddit = 'kitty';
 
-  return image.resolutions
-    .filter(resolution => resolution.width === 216)
-    .map(resolution => resolution.url)[0];
-});
+fetch(`https://www.reddit.com/r/${subreddit}/hot.json?raw_json=1`)
+  .then(response => response.json())
+  .then(response => {
+    return response.data.children.map(child => {
+      // get the first image
+      const image = child.data.preview.images[0];
 
-console.log(JSON.stringify(thumbnails, null, 2));
+      return image.resolutions
+        .filter(resolution => resolution.width === 216)
+        .map(resolution => resolution.url)[0];
+    });
+  })
+  .then(thumbnails => console.log(JSON.stringify(thumbnails, null, 2)))
