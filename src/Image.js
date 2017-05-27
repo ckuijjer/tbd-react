ff76262da1c81f94d387f320b09d5d3b29588e1f
Casper Kuijjer <casper@kuijjer.com>
2017-05-27 11:58:06 +0200

    Comment

--- src/Image.js
+++ src/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Image = ({ src, onClick = () => {} }) => {
   const styles = {
-    paddingBottom: '100%',
+    paddingBottom: '100%', // percentage is based on the width
     background: `url(${src}) center / cover`
   };
 
