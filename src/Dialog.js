df253b826e929f93f865f8f46e5a8b215c67c8a9
Casper Kuijjer <casper@kuijjer.com>
2017-05-27 13:04:44 +0200

    Close the dialog using escape

--- src/Dialog.js
+++ src/Dialog.js
@@ -1,29 +1,55 @@
 import React from 'react';
 
-const Dialog = ({ isOpen, children, onClose = () => {}, style }) => {
-  if (!isOpen) return null;
+class Dialog extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const styles = {
-    overlay: {
-      backgroundColor: 'rgba(255,255,255,0.8)',
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  };
+    this.onEscape = this.onEscape.bind(this);
+  }
 
-  return (
-    <div style={styles.overlay} onClick={onClose}>
-      <div style={style}>
-        { children }
+  componentDidMount() {
+    document.addEventListener('keydown', this.onEscape);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.onEscape);    
+  }
+
+  onEscape = (event) => {
+    const { isOpen, onClose = () => {} } = this.props;
+
+    if (event.keyCode === 27 && isOpen) {
+      onClose();
+    }
+  }
+
+  render() {
+    const { isOpen, children, onClose = () => {}, style } = this.props;
+
+    if (!isOpen) return null;
+
+    const styles = {
+      overlay: {
+        backgroundColor: 'rgba(255,255,255,0.8)',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    };
+
+    return (
+      <div style={styles.overlay} onClick={onClose}>
+        <div style={style}>
+          { children }
+        </div>
       </div>
-    </div>
-  )
-};
+    );
+  }
+}
 
 export default Dialog;
