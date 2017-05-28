a616527f12532ad463d047cd87a44e00da72819f
Casper Kuijjer <casper@kuijjer.com>
2017-05-28 22:05:52 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+  Link
+} from 'react-router-dom'
 
 import GalleryContainer from './GalleryContainer';
 
-class App extends Component {
+class App extends React.Component {
   render() {
-    return <GalleryContainer />
+    return (
+      <Router>
+        <Switch>
+          <Route path="/:subreddit" component={GalleryContainer} />
+          <Redirect from="/" to="/kitten" />
+        </Switch>
+      </Router>
+    )
   }
 }
 
